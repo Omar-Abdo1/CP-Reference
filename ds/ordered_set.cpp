bd924a8c0b32fbc5b6e6328b3c2566039c045733
OmarAbdo <113479845+Omar-Abdo1@users.noreply.github.com>
2025-09-09 18:11:28 +0300

    Update ordered_set.cpp

---
+++ ds/ordered_set.cpp
@@ -0,0 +1,9 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+#define ordered_set tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>
+//order_of_key (k) : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero). 0-based
+
+// ordered_set<int> ,  ordered_set<string>  etc...
