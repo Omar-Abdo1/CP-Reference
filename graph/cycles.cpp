4be0933fe36e53cb279d2e0f4d41b6640fd42fee
OmarAbdo <113479845+Omar-Abdo1@users.noreply.github.com>
2025-09-10 03:42:29 +0300

    Update cycles.cpp

---
+++ graph/cycles.cpp
@@ -0,0 +1,54 @@
+const int N = 100000;
+
+// variables to be used
+// in both functions
+vector<int> graph[N];
+vector<vector<int>> cycles;
+
+// Function to mark the vertex with
+// different colors for different cycles
+void dfs_cycle(int u, int p, int color[], int par[], int& cyclenumber)
+{
+
+	// already (completely) visited vertex.
+	if (color[u] == 2) {
+		return;
+	}
+
+	// seen vertex, but was not completely visited -> cycle detected.
+	// backtrack based on parents to find the complete cycle.
+	if (color[u] == 1) {
+		vector<int> v;
+		cyclenumber++;
+		
+		int cur = p;
+		v.push_back(cur);
+
+		// backtrack the vertex which are
+		// in the current cycle thats found
+		while (cur != u) {
+			cur = par[cur];
+			v.push_back(cur);
+		}
+		cycles.push_back(v);
+		return;
+	}
+	par[u] = p;
+
+	// partially visited.
+	color[u] = 1;
+
+	// simple dfs on graph
+	for (int v : graph[u]) {
+
+		// if it has not been visited previously
+		if (v == par[u]) {
+			continue;
+		}
+		dfs_cycle(v, u, color, par, cyclenumber);
+	}
+
+	// completely visited.
+	color[u] = 2;
+}
+
